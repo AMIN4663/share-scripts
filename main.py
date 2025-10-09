65c8b45f292ba1bf19055ad57aa32d7cdf2d183e
Matin Fekri <fekridotwork@gmail.com>
2025-10-09 16:16:43 +0330

    Added q1 with sorted

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-# Day one
-print("This is our First Team Work")
+name_input = input("enter names ")
+
+name_list = name_input.split(',')
+
+#print(name_list)
+
+filtered_name = sorted([ name for name in name_list  if len(name) > 3 and name[0].lower() == "a" ])

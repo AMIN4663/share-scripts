891543053fa8c7e6aeb65e09716af6121f797cda
DP-2006 <danialpourmehdi@gmail.com>
2025-10-10 09:24:06 +0330

    Update phantom-1.py

--- PythonProject2/phantom-1.py
+++ PythonProject2/phantom-1.py
@@ -4,4 +4,3 @@ name_list = name_input.split(',')
 print(name_list)
 filtered_name = [ name for name in name_list  if len(name) > 3 and name[0].lower() == "a" ]
 print(filtered_name)
-#git@github.com:AMIN4663/share-scripts.git

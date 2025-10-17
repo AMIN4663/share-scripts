70d6b9ff4e2b5898f8740d6c74f2249a5a8ea37b
AMIN <m.a.mirani4742@gmail.com>
2025-10-17 17:04:03 +0330

    rename-file

--- oop2.q3.py
+++ oop2.q3.py
@@ -1,21 +1,4 @@
-# #Q.1
-# class user:
-#     all_users = []
-#     def __init__(self,name,):
-#         self.name = name
-#         user.all_users.append(self)
-#
-#     @classmethod
-#     def get_user_count(cls):
-#         return len(cls.all_users)
-# u1 = user('Alic')
-# u2 = user('Bob')
-# print(user.get_user_count())
-#
-#
-#
-
-
+#Q.1
 
 #Q.3
 class car:
